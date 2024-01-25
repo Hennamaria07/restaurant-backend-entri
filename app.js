7fc8d6f240ec3a8714cc0a9fd422910407cb4313
Henna Maria <hennamaria2001@gmail.com>
2024-01-25 16:09:44 +0530

    path fixed

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.static('uploads'));
 app.use(cookieParser());
 app.use(express.json()); // Parse JSON data
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded data
-app.use(__dirname + "/uploads", express.static(__dirname + 'uploads'));
+app.use(__dirname + "/uploads", express.static(__dirname + '/uploads'));
 app.use(cors({ credentials: true, origin: true }));
 app.use("/api/v1", userRoutes);
 app.use("/api/v1", restaurantRoutes);
