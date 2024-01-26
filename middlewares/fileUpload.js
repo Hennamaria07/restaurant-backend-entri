f6ee4a4e252b2449733611e3118367eae954f964
Henna Maria <hennamaria2001@gmail.com>
2024-01-26 12:23:56 +0530

    fixed v1.1.2

--- middlewares/fileUpload.js
+++ middlewares/fileUpload.js
@@ -3,13 +3,12 @@ const path = require('path');
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, path.join(__dirname, "uploads", "restaurantPhotograph"));
+        cb(null, (__dirname + "../../uploads/restaurantPhotograph"));
     },
     filename: (req, file, cb) => {
         cb(null, Date.now() + path.extname(file.originalname))
     }
 });
-
-const upload = multer({ storage });
+const upload = multer({storage});
 
 module.exports = upload;
